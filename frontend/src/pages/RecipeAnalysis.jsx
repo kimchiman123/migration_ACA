53dd58cab23cf8f8b929c615342f3624d765b615
KonoPark <kpark338@naver.com>
2026-01-29 09:52:36 +0900

    useRef 추가

--- frontend/src/pages/RecipeAnalysis.jsx
+++ frontend/src/pages/RecipeAnalysis.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import axiosInstance from '../axiosConfig';
