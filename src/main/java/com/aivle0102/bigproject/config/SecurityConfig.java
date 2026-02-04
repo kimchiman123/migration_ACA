e679e4680228b1010f1a12029cb1267d83f34793
kh2910 <kh2910@naver.com>
2026-02-04 14:03:17 +0900

    프록시 설정

--- src/main/java/com/aivle0102/bigproject/config/SecurityConfig.java
+++ src/main/java/com/aivle0102/bigproject/config/SecurityConfig.java
@@ -76,7 +76,10 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
-                configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
+                configuration.setAllowedOrigins(List.of(
+                                "http://localhost:3000",
+                                "http://localhost:5173",
+                                "https://bp-frontend-app.wittysand-a0f4e87e.centralindia.azurecontainerapps.io"));
                 configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 configuration.setAllowedHeaders(List.of("*"));
                 configuration.setAllowCredentials(true);
